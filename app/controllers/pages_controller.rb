cd0eb589d85cc75b2eb4f25f4f99e785be9166cd
gerson <gersonrevatta@gmail.com>
2018-11-17 18:07:36 -0500

    Transform html data

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,15 @@
 class PagesController < ApplicationController
+  require 'httparty'
+  require 'nokogiri'
+  require 'pry'
   def home
-    
+    pagina = HTTParty.get('https://madrid.craigslist.es/search/hhh?=0')
+    @array_Pisos = []
+    paginaNokogiri = Nokogiri::HTML(pagina)
+    paginaNokogiri.css('.content').css('.rows').css('.hdrlnk').map do |a|
+      name = a.text
+      Product.create(name: name)
+    end
+    @products = Product.all
   end
 end
